6469d33566c2b5656868f02765f8ef037635087e
shvgpt-070208 <shvgpt070208@gmail.com>
2026-04-16 01:10:28 +0530

    feat: added domain in cookies

--- apps/server/src/config/cookie-options.ts
+++ apps/server/src/config/cookie-options.ts
@@ -4,5 +4,7 @@ export const cookieOptions: CookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: "none",
-  maxAge: 7 * 24 * 60 * 60 * 1000,
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+  domain:
+    process.env.NODE_ENV === "production" ? ".bitsketcher.dev" : undefined,
 }
